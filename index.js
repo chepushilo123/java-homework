c96bbc5a0f53c9d77319c3e7b2eb5173515cd00e
chepushilo123 <Lleha583@gmail.com>
2024-02-16 03:14:53 +0300

    function

--- index.js
+++ index.js
@@ -1,248 +1,158 @@
-//Задача 1
+/*
 
-var x = 20;
-var y = 58;
-var z = 42;
-var summ = x + y + z;
-console.log(summ)
+//задача 1
 
+var a = prompt('Введите число', 100);
 
+function getSum(a) {
 
+    var sum = 0;
 
-
-//Задача 2
-
-var year;
-var hours;
-var minuts;
-var myAgeInSeconds;
-
-year = prompt('Укажите ваш возраст', 20)
-
-year = Number(year);
-
-hours = year * 8760;
-console.log(hours)
-minuts = hours * 60;
-console.log(minuts)
-myAgeInSeconds = minuts * 60;
-console.log(myAgeInSeconds)
-
-alert('вам ' + myAgeInSeconds + ' в секундах')
-
-
-
-
-
-//Задача 3
-
-var count = 42
-var userName = '42'
-
-count = String(count)
-userName = Number(userName)
-
-
-//второй способ
-
-var a = count;
-count = userName;
-userName = a;
-
-
-
-
-
-//Задача 4
-
-var a = 1
-var b = 2
-var c = ' белых медведей'
-
-a = String(a)
-b = String(b)
-
-var together = a + b + c;
-
-console.log(together)
-
-
-
-
-
-//Задача 5
-var vname = 'vasya';
-var number = 17;
-var bol = true;
-
-console.log('Variable: ' + vname + ' have type: ' + typeof(vname))
-console.log('Variable: ' + number + ' have type: ' + typeof(number))
-console.log('Variable: ' + bol + ' have type: ' + typeof(bol))
-
-
-
-
-
-
-//Задача 6
-
-var firstname;
-var lastname;
-
-firstname = prompt('Укажите ваше имя')
-lastname = prompt('Укажите вашу фамилию')
-
-firstname = firstname + ' ';
-
-console.log('Добрый день, ' + firstname + lastname)
-alert('Добрый день, ' + firstname + lastname)
-
-
-
-
-
-//Задача 7
-
-var a = 4;
-var b = 3;
-
-a = --a;
-b = ++b;
-
-console.log(a)
-console.log(b)
-
-
-
-
-
-//Задача 8
-
-var str = 'true';
-var bool = false;
-var num = 17;
-var undef = undefined;
-var nul = null;
-
-console.log(typeof(str))
-console.log(typeof(bool))
-console.log(typeof(num))
-console.log(typeof(undef))
-console.log(typeof(nul))
-
-
-
-
-
-//Задача 9
-
-var height = 15;
-var width = 20;
-
-if (height > width) {
-    console.log(height)
-} else {
-    console.log(width)
-}
-
-
-
-
-
-//Задача 10
-
-for(i = 0; i < 20; i++) {
-    if (i % 3 === 0) {
-        console.log(i)
+    for (i = 0; i <= a; i++) {
+        sum = sum + i;
     }
+
+    console.log(sum)
 }
 
+getSum(a)
 
 
 
-//Задача 11
 
-var key = true;
-var documents = true;
-var pen = true;
-var apple = true;
-var orange = false;
-var shouldGoToWork;
+//задача 2
 
-if (key && documents && pen && apple && orange === true) {
-    shouldGoToWork = true;
-    console.log(shouldGoToWork)
-} else {
-    shouldGoToWork = false;
-    console.log(shouldGoToWork)
+var money = prompt('какую сумму вы хотите взять?');
+money = Number(money);
+
+function value(money) {
+    var pereplata;
+    var allSum;
+    var month;
+
+    pereplata = money * 17 / 100;
+    allSum = (money + pereplata);
+    month = allSum / 5 / 12;
+    
+
+    console.log('вся сумма составит ' + allSum)
+    console.log('переплата составит ' + pereplata)
+    console.log('ежемесячная оплата составит ' + month)
 }
 
+value(money)
 
 
 
 
-//Задача 12
+//задача 3
 
-var number;
-number = prompt('Введите число', 30)
-number = Number(number)
+var str = prompt('yours word');
+var from = prompt('from');
+var to = prompt('to');
+
+console.log(str)
+
+function trimString(str, from, to) {
+    
+    var n = '';
+    
+    while ( from != to) {
+        n = n + str[from];
+        from++;
+    }
+
+    console.log(n)
+}
+trimString(str, from, to)
+
+
+
+
+//задача 4
+
+
+var number = prompt('ваше число', 2021);
+
 console.log(number)
 
-if (number % 3 === 0  && number % 5 === 0) {
-    console.log('FizBuz')
-} else {
-    if (number % 3 === 0) {
-        console.log('Fiz')
+function getSumNumbers(number) {
+    var sum = 0;
+    var c = 0;
+
+    for ( i = 0; i < number.length; i++) {
+
+        c = number[i];
+        c = Number(c);
+
+        sum = sum + c;
     }
 
-    if (number % 5 === 0 ) {
-        console.log('Buz')
-    }
+    console.log(sum)
 }
 
+getSumNumbers(number)
 
 
 
 
-//Задача 13
+//задача 5
 
-var age;
-age = prompt('Укажите свой возраст')
-age = Number(age)
+var a = prompt('a = ?', 1);
+var b = prompt('b = ?', 1);
 
-if (age >= 18) {
-    alert('Попей пивка.')
-} else {
-    if (age <= 15) {
-        alert('Пей колу')
+a = Number(a);
+b = Number(b);
+
+
+function getSum(a, b) {
+    var sum = 0;
+
+    if (a === b) {
+        console.log('Since both are same')
+        return a;
+    }
+
+    if (a > b) {
+        var temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (i = a; i <= b; i++) {
+        sum += i;
+        a++;
+    }
+
+    console.log(sum)
+}
+
+getSum(a, b)
+
+*/
+
+
+
+//задача 6
+
+var bool = confirm('true or false');
+
+function fooboo(bool) {
+    if (bool == true) {
+        foo()
     } else {
-        if (age === 16 || 17 || 18){
-            alert('Можешь выкурить сигаретку, только маме не говори')
-        }
+        boo()
     }
 }
 
-
-
-
-//Задача 14
-
-var country;
-country = prompt('В какую сторону света вы бы хотели отправиться?', 'восток')
-
-switch(country) {
-    case 'юг':
-        alert('на юг пойдешь счастье найдешь')
-        break;
-    case 'север':
-        alert('на север пойдешь много денег найдешь')
-        break;
-    case 'запад':
-        alert('на запад пойдешь верного друга найдешь')
-        break;
-    case 'восток':
-        alert('на восток пойдешь разработчиком станешь')
-        break;
-    default:
-        console.log('Попробуйте ещё раз')
+function foo() {
+    console.log('foo')
+    return;
 }
+
+function boo() {
+    console.log('boo')
+    return;
+}
+
+fooboo(bool)
